2bed71da39e6029c17e9a4cfd8285e49ae0b208e
darknews <49564491+darknews23@users.noreply.github.com>
2021-11-07 00:28:25 +0300

    init commit (#1)
    Co-authored-by: Кузьмин Дмитрий Вячеславович <Dmitry.Kuzmin@norbit.ru>

---
+++ DAL/Data/LifeGameContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using DAL.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Data
+{
+    public class LifeGameContext : IdentityDbContext<SecurityUser, SecurityRole, Guid, IdentityUserClaim<Guid>, IdentityUserRole<Guid>, IdentityUserLogin<Guid>, IdentityRoleClaim<Guid>, IdentityUserToken<Guid>>
+    {
+        public LifeGameContext(DbContextOptions<LifeGameContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
