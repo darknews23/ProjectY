c2ce1499c4695b350434c7f6108533e0ef84745b
Кузьмин Дмитрий Вячеславович <Dmitry.Kuzmin@norbit.ru>
2021-11-06 23:48:45 +0300

    init commit

---
+++ DAL/Data/Entities/SecurityRole.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+
+namespace DAL.Data.Entities
+{
+    public class SecurityRole : IdentityRole<Guid>
+    {
+    }
+}
---
+++ DAL/Data/Entities/SecurityUser.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace DAL.Data.Entities
+{
+    public class SecurityUser : IdentityUser<Guid>
+    {
+
+    }
+}
